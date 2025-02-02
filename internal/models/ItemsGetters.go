db2dac5795f5f2227d3bd782a4be40a71b6fd7ef
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-02-01 21:06:10 -0500

    rename file to better match it's behavior

--- internal/models/ItemsGetters.go
+++ internal/models/ItemsGetters.go
@@ -6,6 +6,18 @@ import (
 	"mathing/internal/store"
 )
 
+func (i *ItemsList) CurrentPage() int64 {
+	return (i.pageOffset / 20) + 1
+}
+
+func (i *ItemsList) PageCount() int64 {
+	return int64(math.Ceil(float64(i.itemCount) / 20))
+}
+
+func (i *ItemsList) CurrentItem() store.Item {
+	return i.items[i.selected]
+}
+
 func (i *ItemsList) Refetch() (err error) {
 	ctx := context.Background()
 	_, i.data, err = i.store.GetItemTable(ctx, i.pageOffset)
@@ -25,16 +37,4 @@ func (i *ItemsList) Refetch() (err error) {
 
 	i.selected = 0
 	return nil
-}
-
-func (i *ItemsList) CurrentPage() int64 {
-	return (i.pageOffset / 20) + 1
-}
-
-func (i *ItemsList) PageCount() int64 {
-	return int64(math.Ceil(float64(i.itemCount) / 20))
-}
-
-func (i *ItemsList) CurrentItem() store.Item {
-	return i.items[i.selected]
 }
